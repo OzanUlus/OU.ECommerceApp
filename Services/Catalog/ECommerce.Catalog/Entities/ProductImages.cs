ac668294eca97c8c6ed093babe9391f86eebc14f
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-22 23:21:48 +0300

    ımage and detail

---
+++ Services/Catalog/ECommerce.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,12 @@
+﻿namespace ECommerce.Catalog.Entities
+{
+    public class ProductImages
+    {
+        public string ProductImagesId { get; set; }
+        public string Images1 { get; set; }
+        public string Images2 { get; set; }
+        public string Images3 { get; set; }
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
