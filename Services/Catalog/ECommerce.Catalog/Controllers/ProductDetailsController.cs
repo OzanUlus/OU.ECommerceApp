637786c5af2fc736e807545d69651c456c5ead6f
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-23 12:26:40 +0300

    product and productdetail controllers

---
+++ Services/Catalog/ECommerce.Catalog/Controllers/ProductDetailsController.cs
@@ -0,0 +1,52 @@
+﻿using ECommerce.Catalog.Dtos.ProductDetailDtos;
+using ECommerce.Catalog.Services.ProductDetailServices;
+using ECommerce.Catalog.Services.ProductDetailServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductDetailsController : ControllerBase
+    {
+        private readonly IProductDetailService _productDetailService;
+
+        public ProductDetailsController(IProductDetailService productDetailService)
+        {
+            _productDetailService = productDetailService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductDetailList()
+        {
+            var response = await _productDetailService.GetAllAsync();
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductDetailById(string id)
+        {
+            var response = await _productDetailService.GetByIdProductDetailAsync(id);
+            return Ok(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProductDetail(CreateProductDetailDto createProductDetailDto)
+        {
+            await _productDetailService.CreateProductDetailAsync(createProductDetailDto);
+            return Ok("Ürün detayları başarı ile eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProductDetail(string id)
+        {
+            await _productDetailService.DeleteProductDetailAsync(id);
+            return Ok("Ürün detayları başarı ile silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProductDetail(UpdateProductDetailDto updateProductDetailDto)
+        {
+            await _productDetailService.UpdateProductDetailAsync(updateProductDetailDto);
+            return Ok("Ürün detayları başarı ile güncellendi");
+        }
+    }
+}
---
+++ Services/Catalog/ECommerce.Catalog/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using ECommerce.Catalog.Dtos.ProductDtos;
+using ECommerce.Catalog.Services.ProductServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ProductList()
+        {
+            var response = await _productService.GetAllAsync();
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetProductById(string id)
+        {
+            var response = await _productService.GetByIdProductAsync(id);
+            return Ok(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateProduct(CreateProductDto createProductDto)
+        {
+            await _productService.CreateProductAsync(createProductDto);
+            return Ok("Ürün başarı ile eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteProduct(string id)
+        {
+            await _productService.DeleteProductAsync(id);
+            return Ok("Ürün başarı ile silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(UpdateProductDto updateProductDto)
+        {
+            await _productService.UpdateProductAsync(updateProductDto);
+            return Ok("Ürün başarı ile güncellendi");
+        }
+    }
+}
