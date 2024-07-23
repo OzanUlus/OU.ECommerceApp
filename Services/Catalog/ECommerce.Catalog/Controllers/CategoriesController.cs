fe3285f81ac668647b074d34ecf5da13cea6c965
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-23 12:20:00 +0300

    category controller

---
+++ Services/Catalog/ECommerce.Catalog/Controllers/CategoriesController.cs
@@ -0,0 +1,51 @@
+﻿using ECommerce.Catalog.Dtos.CategoryDtos;
+using ECommerce.Catalog.Services.CategoryServices;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Catalog.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CategoryList() 
+        {
+          var response = await _categoryService.GetAllAsync();
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCategoryById(string id)
+        {
+            var response = await _categoryService.GetByIdCategoryAsync(id);
+            return Ok(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            await _categoryService.CreateCategoryAsync(createCategoryDto);
+            return Ok("Kategori başarı ile eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteCategory(string id)
+        {
+             await _categoryService.DeleteCategoryAsync(id);
+            return Ok("Kategori başarı ile silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateCategory(UpdateCategoryDto updateCategoryDto)
+        {
+             await _categoryService.UpdateCategoryAsync(updateCategoryDto);
+            return Ok("Kategori başarı ile güncellendi");
+        }
+    }
+}
