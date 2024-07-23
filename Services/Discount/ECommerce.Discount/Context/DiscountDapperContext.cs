a7d35bb3a9936759e137cf36ded79eb69553e6e9
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-24 01:03:57 +0300

    coupon and dbcontext

---
+++ Services/Discount/ECommerce.Discount/Context/DiscountDapperContext.cs
@@ -0,0 +1,28 @@
+﻿using ECommerce.Discount.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using System.Data;
+
+namespace ECommerce.Discount.Context
+{
+    public class DiscountDapperContext : DbContext
+    {
+        private readonly IConfiguration _configuration;
+        private readonly string _connectionString;
+
+        public DiscountDapperContext(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = configuration.GetConnectionString("DefaultConnection")!;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Data Source=ILKEROZAN;Initial Catalog=ECommerceDiscountDb;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+        }
+        public DbSet<Coupon> Coupons { get; set; }
+        public IDbConnection CreateConnection() => new SqlConnection(_connectionString);
+
+    }
+}
