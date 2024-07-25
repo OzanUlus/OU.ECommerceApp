0700a237dd69176f495b7bf2e581af69517e7058
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-25 14:00:17 +0300

    fixed

--- Services/Order/Core/ECommerce.Order.Application/Features/Mediator/Handlers/OrderingHandlers/CreateOrderingCommandHandler.cs
+++ Services/Order/Core/ECommerce.Order.Application/Features/Mediator/Handlers/OrderingHandlers/CreateOrderingCommandHandler.cs
@@ -2,11 +2,6 @@
 using ECommerce.Order.Application.Interfaces;
 using ECommerce.Order.Domain.Entities;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ECommerce.Order.Application.Features.Mediator.Handlers.OrderingHandlers
 {
