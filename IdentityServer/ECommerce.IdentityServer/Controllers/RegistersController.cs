cf6cd5ad9cfa1cd380bb868037467fab4693921a
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-26 17:12:54 +0300

    user registiration

---
+++ IdentityServer/ECommerce.IdentityServer/Controllers/RegistersController.cs
@@ -0,0 +1,42 @@
+﻿using ECommerce.IdentityServer.Dtos;
+using ECommerce.IdentityServer.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace ECommerce.IdentityServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegistersController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public RegistersController(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> UserRegister(UserRegisterDto userRegisterDto)
+        {
+            var user = new ApplicationUser()
+            {
+                Email = userRegisterDto.Email,
+                Name = userRegisterDto.Name,
+                Surname = userRegisterDto.Surname,
+                UserName = userRegisterDto.Username,
+            };
+
+            var result = await _userManager.CreateAsync(user,userRegisterDto.Password);
+
+            if (!result.Succeeded) 
+            {
+                return BadRequest("Kullanıcı oluşturulurken bir hata meydana geldi.");
+            }
+
+            return Ok("Kullanıcı başarıyla oluşturuldu.");
+        }
+    }
+}
