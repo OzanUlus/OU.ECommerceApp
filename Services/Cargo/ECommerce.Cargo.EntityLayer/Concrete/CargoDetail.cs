bb24058768b98990f21a07502e10f98c0ed3012c
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-29 19:09:14 +0300

    cargo entity

---
+++ Services/Cargo/ECommerce.Cargo.EntityLayer/Concrete/CargoDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Cargo.EntityLayer.Concrete
+{
+    public class CargoDetail
+    {
+        public int CargoDetailId { get; set; }
+        public string SenderCustomer { get; set; }
+        public string RecieverCustomer { get; set; }
+        public int Barcode { get; set; }
+        public int CargoCamponyId { get; set; }
+        public CargoCompany CargoCampony { get; set; }
+    }
+}
