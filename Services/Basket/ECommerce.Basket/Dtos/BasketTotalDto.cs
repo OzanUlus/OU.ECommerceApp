f0647e0ea1093e6d19cbbc70d01eccd826cf59b8
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-30 22:51:45 +0300

    baskettotaldto and basketıtemdto

---
+++ Services/Basket/ECommerce.Basket/Dtos/BasketTotalDto.cs
@@ -0,0 +1,11 @@
+﻿namespace ECommerce.Basket.Dtos
+{
+    public class BasketTotalDto
+    {
+        public string UserId { get; set; }
+        public string DiscountCode { get; set; }
+        public int? DiscountRate { get; set; }
+        public List<BasketItemDto> BasketItems { get; set; }
+        public decimal TotalPrice { get => BasketItems.Sum(bi => bi.Price * bi.Quantity); }
+    }
+}
