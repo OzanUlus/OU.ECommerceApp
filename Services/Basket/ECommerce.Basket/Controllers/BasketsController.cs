39466fbb72b1d30264644e856dc0770885f497a2
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-30 23:55:34 +0300

    basket controller

--- Services/Basket/ECommerce.Basket/Controllers/BasketsController.cs
+++ Services/Basket/ECommerce.Basket/Controllers/BasketsController.cs
@@ -1,5 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ECommerce.Basket.Dtos;
+using ECommerce.Basket.LoginServices;
+using ECommerce.Basket.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Runtime.CompilerServices;
 
 namespace ECommerce.Basket.Controllers
 {
@@ -7,5 +11,34 @@ namespace ECommerce.Basket.Controllers
     [ApiController]
     public class BasketsController : ControllerBase
     {
+        private readonly IBasketService _basketService;
+        private readonly ILoginService _loginService;
+
+        public BasketsController(IBasketService basketService, ILoginService loginService)
+        {
+            _basketService = basketService;
+            _loginService = loginService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetMyBasketDetail() 
+        {
+            var values = await _basketService.GetBasket(_loginService.GetUSerId);
+            return Ok(values);
+        }
+        [HttpGet]
+        public async Task<IActionResult> SaveMyBasket(BasketTotalDto basketTotalDto)
+        {
+            basketTotalDto.UserId = _loginService.GetUSerId;
+            await _basketService.SaveBasket(basketTotalDto);
+            return Ok("Sepetteki değişiklikler başarıyla değiştirildi");    
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeleteMyBasket()
+        {
+            await _basketService.DeleteBasket(_loginService.GetUSerId);
+            return Ok("Sepet başarıyla silindi");
+        }
     }
 }
