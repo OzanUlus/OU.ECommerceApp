a6e55cfce09d2fb43cee91e5f8821f0b74629276
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-31 16:30:19 +0300

    _CategoriesDefaultComponent

---
+++ Frontends/ECommerce.WebUı/ViewComponents/DefaultViewComponents/_CategoriesDefaultComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.ViewComponents.DefaultViewComponents
+{
+    public class _CategoriesDefaultComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }    
+    }
+}
