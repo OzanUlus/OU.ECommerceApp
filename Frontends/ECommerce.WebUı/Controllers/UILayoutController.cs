d247278556c2d75366172d43359f0ac82d6eb6d2
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-31 13:23:57 +0300

    design uı

---
+++ Frontends/ECommerce.WebUı/Controllers/UILayoutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Controllers
+{
+    public class UILayoutController : Controller
+    {
+        public IActionResult _UILayout()
+        {
+            return View();
+        }
+    }
+}
