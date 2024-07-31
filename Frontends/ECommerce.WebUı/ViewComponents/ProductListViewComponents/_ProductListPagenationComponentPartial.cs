00864b0e58a00963843a4707bc6525e5d1d0e325
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-31 22:11:07 +0300

    pagenation

---
+++ Frontends/ECommerce.WebUı/ViewComponents/ProductListViewComponents/_ProductListPagenationComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.ViewComponents.ProductListViewComponents
+{
+    public class _ProductListPagenationComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }
+    }
+}
