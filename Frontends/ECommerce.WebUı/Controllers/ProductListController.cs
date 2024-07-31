5bad0513153a2135e81a724341c3dd3a9a76ed29
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-31 20:51:13 +0300

    ürünlist page

---
+++ Frontends/ECommerce.WebUı/Controllers/ProductListController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Controllers
+{
+    public class ProductListController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
