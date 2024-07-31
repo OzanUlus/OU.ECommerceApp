269d0727f074403c300a5278c635da4d0df57829
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-31 14:18:15 +0300

    _TopbarUILayoutComponentPartial

---
+++ Frontends/ECommerce.WebUı/ViewComponents/UILayoutViewComponents/_TopbarUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.ViewComponents.UILayoutViewComponents
+{
+    public class _TopbarUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }
+    }
+}
