957570c584ba339b1131c66cd8bd3fdece47e737
Ozan Ulus <ilkerozanulus@gmail.com>
2024-07-31 20:39:36 +0300

    _ScriptUILayoutComponentPartial

---
+++ Frontends/ECommerce.WebUı/ViewComponents/UILayoutViewComponents/_ScriptUILayoutComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.ViewComponents.UILayoutViewComponents
+{
+    public class _ScriptUILayoutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        {
+            return View();
+        }    
+    }
+}
