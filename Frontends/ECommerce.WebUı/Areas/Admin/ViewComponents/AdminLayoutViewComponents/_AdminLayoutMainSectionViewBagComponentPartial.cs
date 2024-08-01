553eba332a3c2886fdd9b9f19241c1693489d953
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-01 17:34:07 +0300

    category crud operation consium from api at mvc

---
+++ Frontends/ECommerce.WebUı/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutMainSectionViewBagComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutMainSectionViewBagComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/ECommerceApp.DtoLayer/CatologDtos/CategoryDtos/CreateCategoryDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerceApp.DtoLayer.CatologDtos.CategoryDtos
+{
+    public class CreateCategoryDto
+    {
+        public string CategoryName { get; set; }
+    }
+}
