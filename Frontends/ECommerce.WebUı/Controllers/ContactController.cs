26b5c7c3b18958cf8af7228a24b3f9ba11102951
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-01 12:25:50 +0300

    _ContactDetailComponentPartial

---
+++ Frontends/ECommerce.WebUı/Controllers/ContactController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/ECommerce.WebUı/ViewComponents/ContactViewComponent/_ContactDetailComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.ViewComponents.ContactViewComponent
+{
+    public class _ContactDetailComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View();
+        }
+    }
+}
