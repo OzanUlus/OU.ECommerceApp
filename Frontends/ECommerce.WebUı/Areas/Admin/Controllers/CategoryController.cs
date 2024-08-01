23dfc929066a23c81e19acc962fb9589e4f99ab2
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-01 13:57:05 +0300

    _AdminLayoutHeaderComponentPartial and _AdminLayoutHeadComponentPartial

---
+++ Frontends/ECommerce.WebUı/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Areas.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/ECommerce.WebUı/Areas/Admin/ViewComponents/AdminLayoutViewComponents/_AdminLayoutHeadComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Areas.Admin.ViewComponents.AdminLayoutViewComponents
+{
+    public class _AdminLayoutHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+
+            return View();
+        }
+    }
+}
