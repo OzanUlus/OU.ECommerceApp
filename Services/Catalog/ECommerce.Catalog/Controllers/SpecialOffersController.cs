c290e0ff09f7da5647400755ca6dfd52fbe8e2c5
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-06 11:35:58 +0300

    specialoffer controller and test api

---
+++ Services/Catalog/ECommerce.Catalog/Controllers/SpecialOffersController.cs
@@ -0,0 +1,53 @@
+﻿using ECommerce.Catalog.Dtos.SpecailOfferDtos;
+using ECommerce.Catalog.Services.SpecialOfferServices;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Catalog.Controllers
+{
+    [AllowAnonymous]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SpecialOffersController : ControllerBase
+    {
+        private readonly ISpecialOfferService _specialOfferService;
+
+        public SpecialOffersController(ISpecialOfferService specialOfferService)
+        {
+            _specialOfferService = specialOfferService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> SpecialOfferList()
+        {
+            var response = await _specialOfferService.GetAllAsync();
+            return Ok(response);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetSpecialOfferById(string id)
+        {
+            var response = await _specialOfferService.GetByIdSpecialOfferAsync(id);
+            return Ok(response);
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateSpecialOffer(CreateSpecialOfferDto createSpecialOfferDto)
+        {
+            await _specialOfferService.CreateSpecialOfferAsync(createSpecialOfferDto);
+            return Ok("Özel teklif başarı ile eklendi");
+        }
+        [HttpDelete]
+        public async Task<IActionResult> DeleteSpecialOffer(string id)
+        {
+            await _specialOfferService.DeleteSpecialOfferAsync(id);
+            return Ok("Özel teklif başarı ile silindi");
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateSpecialOffer(UpdateSpecialOfferDto updateSpecialOfferDto)
+        {
+            await _specialOfferService.UpdateSpecialOfferAsync(updateSpecialOfferDto);
+            return Ok("Özel teklif başarı ile güncellendi");
+        }
+    }
+}
--- Services/Catalog/ECommerce.Catalog/Dtos/SpecailOfferDtos/CreateSpecialOfferDto.cs
+++ Services/Catalog/ECommerce.Catalog/Dtos/SpecailOfferDtos/CreateSpecialOfferDto.cs
@@ -2,7 +2,7 @@
 {
     public class CreateSpecialOfferDto
     {
-        public string SpecialOfferId { get; set; }
+        
         public string Title { get; set; }
         public string Subtitle { get; set; }
         public string ImageUrl { get; set; }
