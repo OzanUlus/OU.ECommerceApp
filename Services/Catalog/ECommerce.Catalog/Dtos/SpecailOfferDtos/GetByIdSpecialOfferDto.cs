30d1f17f17b091628b2e8ce738830f819b765981
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-06 10:31:34 +0300

    specialoffer entity and dtos

---
+++ Services/Catalog/ECommerce.Catalog/Dtos/SpecailOfferDtos/GetByIdSpecialOfferDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.Catalog.Dtos.SpecailOfferDtos
+{
+    public class GetByIdSpecialOfferDto
+    {
+        public string SpecialOfferId { get; set; }
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
