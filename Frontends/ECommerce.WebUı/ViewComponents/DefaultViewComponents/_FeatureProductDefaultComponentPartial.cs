d7c63461d94416cc53da538a1c4842964efea36c
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-07 14:08:06 +0300

    dynmic product

--- Frontends/ECommerce.WebUı/ViewComponents/DefaultViewComponents/_FeatureProductDefaultComponentPartial.cs
+++ Frontends/ECommerce.WebUı/ViewComponents/DefaultViewComponents/_FeatureProductDefaultComponentPartial.cs
@@ -1,11 +1,32 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerceApp.DtoLayer.CatologDtos.PrdouctDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace ECommerce.WebUı.ViewComponents.DefaultViewComponents
 {
     public class _FeatureProductDefaultComponentPartial : ViewComponent
     {
-        public IViewComponentResult Invoke() 
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _FeatureProductDefaultComponentPartial(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+      
+
+        public async Task<IViewComponentResult> InvokeAsync() 
+        {
+
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:7070/api/Products");
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var values = JsonConvert.DeserializeObject<List<ResultProductDto>>(jsonData);
+                return View(values);
+            }
             return View();
         }
     }
