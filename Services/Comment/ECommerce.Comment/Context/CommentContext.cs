a0cf48a7a178b8c81f44cdf8d346da496d4e3e70
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-12 17:45:56 +0300

    comment entity and context

---
+++ Services/Comment/ECommerce.Comment/Context/CommentContext.cs
@@ -0,0 +1,15 @@
+﻿using ECommerce.Comment.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ECommerce.Comment.Context
+{
+    public class CommentContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost,1442;Database=ECommerceCommentDb;User=sa;Password=Password12*;TrustServerCertificate=True");
+        }
+
+        public DbSet<UserComment> UserComments { get; set; }
+    }
+}
