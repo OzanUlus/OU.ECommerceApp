383ed7d33da7cd37609e3f213421357aedf2b661
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-12 18:05:39 +0300

    coomment controller

---
+++ Services/Comment/ECommerce.Comment/Controllers/CommentsController.cs
@@ -0,0 +1,60 @@
+﻿using ECommerce.Comment.Context;
+using ECommerce.Comment.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.Comment.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class CommentsController : ControllerBase
+    {
+        private readonly CommentContext _commentContext;
+
+        public CommentsController(CommentContext commentContext)
+        {
+            _commentContext = commentContext;
+        }
+
+        [HttpGet]
+        public IActionResult CommentList() 
+        { 
+          var values = _commentContext.UserComments.ToList();
+            return Ok(values);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCommentById(int id)
+        {
+            var value = _commentContext.UserComments.Find(id);
+            return Ok(value);
+        }
+
+        [HttpPost]
+        public IActionResult CreateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Add(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarı ile eklendi");
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteComment(int id)
+        {
+          var value =  _commentContext.UserComments.Find(id);
+            _commentContext.UserComments.Remove(value);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarı ile silindi");
+        }
+
+        [HttpPut]
+        public IActionResult UpdateComment(UserComment userComment)
+        {
+            _commentContext.UserComments.Update(userComment);
+            _commentContext.SaveChanges();
+            return Ok("Yorum başarı ile güncellendi");
+        }
+    }
+}
