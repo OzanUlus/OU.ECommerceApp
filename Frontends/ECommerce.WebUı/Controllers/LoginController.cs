e424e8241c338700c79f94b46a47a201921b1283
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-14 00:32:31 +0300

    login backend api

---
+++ Frontends/ECommerce.WebUı/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
