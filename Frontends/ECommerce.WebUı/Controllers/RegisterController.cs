75035ea881f975aba0b300d82de915d2ea5d787f
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-13 23:46:29 +0300

    register uı

---
+++ Frontends/ECommerce.WebUı/Controllers/RegisterController.cs
@@ -0,0 +1,38 @@
+﻿using ECommerceApp.DtoLayer.IdentityDtos.RegisterDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Text;
+
+namespace ECommerce.WebUı.Controllers
+{
+    public class RegisterController : Controller
+    {
+
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public RegisterController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateRegisterDto createRegisterDto )
+        {
+            if(createRegisterDto.Password == createRegisterDto.ConfirmPassword)
+            {
+                var client = _httpClientFactory.CreateClient();
+                var jsonData = JsonConvert.SerializeObject(createRegisterDto);
+                StringContent stringContent = new(jsonData, Encoding.UTF8, "application/json");
+                var responseMessage = await client.PostAsync("http://localhost:5001/api/Registers", stringContent);
+
+                if (responseMessage.IsSuccessStatusCode) return RedirectToAction("Index", "Login");
+            }
+ 
+            return View();
+        }
+    }
+}
