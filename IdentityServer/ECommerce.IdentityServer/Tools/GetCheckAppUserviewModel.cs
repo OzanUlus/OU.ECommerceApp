4890762af6f2491e96fd8c5f0cd6f0a406406cce
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-14 16:59:31 +0300

    created manuel token

---
+++ IdentityServer/ECommerce.IdentityServer/Tools/GetCheckAppUserviewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.IdentityServer.Tools
+{
+    public class GetCheckAppUserviewModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+        public bool IsExist { get; set; }
+    }
+}
---
+++ IdentityServer/ECommerce.IdentityServer/Tools/JwtTokenDefaults.cs
@@ -0,0 +1,10 @@
+﻿namespace ECommerce.IdentityServer.Tools
+{
+    public class JwtTokenDefaults
+    {
+        public const string ValidAudience = "http://localhost"; //yayınlayıcı
+        public const string ValidIssuer = "http://localhost";   //dinleyici
+        public const string Key = "O&UECommerce..0102030405Asp.NetCore8.0.8*/+-";
+        public const int Expire = 60;
+    }
+}
