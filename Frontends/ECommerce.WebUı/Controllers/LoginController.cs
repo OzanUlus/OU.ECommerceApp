dfd79034376d5ddc99342483171a7f66dc120c41
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-14 15:18:41 +0300

    login backend

--- Frontends/ECommerce.WebUı/Controllers/LoginController.cs
+++ Frontends/ECommerce.WebUı/Controllers/LoginController.cs
@@ -1,11 +1,66 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ECommerce.WebUı.Models;
+using ECommerceApp.DtoLayer.IdentityDtos.LoginDtos;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
 
 namespace ECommerce.WebUı.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public LoginController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(LoginDto loginDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var content = new StringContent(JsonSerializer.Serialize(loginDto), Encoding.UTF8, "application/json");
+            var responseMessage = await client.PostAsync("http://localhost:5001/api/Logins", content);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();
+                var tokenModel = JsonSerializer.Deserialize<JwtResponseModel>(jsonData, new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+                });
+
+                if (tokenModel != null)
+                {
+                    JwtSecurityTokenHandler handler = new JwtSecurityTokenHandler();
+                    var token = handler.ReadJwtToken(tokenModel.Token);
+                    var claims = token.Claims.ToList();
+
+                    if (tokenModel.Token != null)
+                    {
+                        claims.Add(new Claim("ouecommercetoken", tokenModel.Token));
+                        var claimsIdentity = new ClaimsIdentity(claims, JwtBearerDefaults.AuthenticationScheme);
+                        var authProps = new AuthenticationProperties()
+                        {
+                            ExpiresUtc = tokenModel.ExpireDate,
+                            IsPersistent = true
+                        };
+
+                        await HttpContext.SignInAsync(JwtBearerDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity), authProps);
+                        return RedirectToAction("Index", "Default");
+                    }
+                }
+
+            }
             return View();
         }
     }
---
+++ Frontends/ECommerce.WebUı/Models/JwtResponseModel.cs
@@ -0,0 +1,8 @@
+﻿namespace ECommerce.WebUı.Models
+{
+    public class JwtResponseModel
+    {
+        public string Token { get; set; }
+        public DateTime ExpireDate { get; set; }
+    }
+}
