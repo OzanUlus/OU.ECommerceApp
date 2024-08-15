ed7509979b68ae0149cab4e9dfe4ac2a6c7eb7fd
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-15 14:57:14 +0300

    clientsetting

---
+++ Frontends/ECommerce.WebUı/Settings/ClientSetting.cs
@@ -0,0 +1,16 @@
+﻿namespace ECommerce.WebUı.Settings
+{
+    public class ClientSetting
+    {
+        public Client ECommerceVisitorClient { get; set; }
+        public Client ECommerceManagerClient { get; set; }
+        public Client ECommerceAdminClient { get; set; }
+
+    }
+
+    public class Client 
+    {
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
