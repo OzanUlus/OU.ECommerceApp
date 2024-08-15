aa0beca43aa9c8e73bebead2a9dda6bb137529dd
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-15 14:39:40 +0300

    signup and ıdentityservice

---
+++ Frontends/ECommerce.WebUı/Services/Concretes/IdentityService.cs
@@ -0,0 +1,13 @@
+﻿using ECommerce.WebUı.Services.Interfaces;
+using ECommerceApp.DtoLayer.IdentityDtos.LoginDtos;
+
+namespace ECommerce.WebUı.Services.Concretes
+{
+    public class IdentityService : IIdentityService
+    {
+        public Task<bool> SignIn(SignUpDto signUpDto)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Frontends/ECommerce.WebUı/Services/Interfaces/IIdentityService.cs
@@ -0,0 +1,9 @@
+﻿using ECommerceApp.DtoLayer.IdentityDtos.LoginDtos;
+
+namespace ECommerce.WebUı.Services.Interfaces
+{
+    public interface IIdentityService
+    {
+        Task<bool> SignIn(SignUpDto signUpDto);
+    }
+}
