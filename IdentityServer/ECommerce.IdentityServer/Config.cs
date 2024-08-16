755e0b64bb5ebc37e85227bf6d21c9c99762bfa9
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-16 15:04:32 +0300

    config managerprofile

--- IdentityServer/ECommerce.IdentityServer/Config.cs
+++ IdentityServer/ECommerce.IdentityServer/Config.cs
@@ -68,7 +68,12 @@ namespace ECommerce.IdentityServer
                ClientName = "ECommerce Manager User",
                AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                ClientSecrets = {new Secret("ecommercesecret".Sha256())},
-               AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission","OcelotFullPermission", "CommentFullPermission", "PaymentFullPermission", "ImagesFullPermission" }
+               AllowedScopes = { "CatalogReadPermission", "CatalogFullPermission", "BasketFullPermission","OcelotFullPermission", "CommentFullPermission", "PaymentFullPermission", "ImagesFullPermission",
+                  IdentityServerConstants.LocalApi.ScopeName,
+                  IdentityServerConstants.StandardScopes.Email,
+                  IdentityServerConstants.StandardScopes.OpenId,
+                  IdentityServerConstants.StandardScopes.Profile
+              }
 
           },
 
