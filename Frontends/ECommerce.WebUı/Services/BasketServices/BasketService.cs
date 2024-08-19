de4a79b2efd309b8de7ba7a5c3718fe9e3d3a6ab
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-19 20:49:06 +0300

    basket service uı

--- Frontends/ECommerce.WebUı/Services/BasketServices/BasketService.cs
+++ Frontends/ECommerce.WebUı/Services/BasketServices/BasketService.cs
@@ -1,6 +1,59 @@
-﻿namespace ECommerce.WebUı.Services.BasketServices
+﻿using ECommerceApp.DtoLayer.BasketDtos;
+
+namespace ECommerce.WebUı.Services.BasketServices
 {
-    public class BasketService
+    public class BasketService : IBasketService
     {
+        private readonly HttpClient _httpClient;
+
+        public BasketService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task AddBasketItem(BasketItemDto basketItemDto)
+        {
+            var value = await GetBasket();
+            if (value != null)
+            {
+                if (!value.BasketItems.Any(bi => bi.ProductId == basketItemDto.ProductId))
+                {
+                    value.BasketItems.Add(basketItemDto);
+                }
+                else
+                {
+                    value = new BasketTotalDto();
+                    value.BasketItems.Add(basketItemDto);
+                }
+
+            }
+            await SaveBasket(value);
+        }
+
+        public Task DeleteBasket(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<BasketTotalDto> GetBasket()
+        {
+            var responseMessage = await _httpClient.GetAsync("baskets");
+            var values = await responseMessage.Content.ReadFromJsonAsync<BasketTotalDto>();
+            return values;
+        }
+
+        public async Task<bool> RemoveBasketItem(string productId)
+        {
+            var values = await GetBasket();
+            var deletedItem = values.BasketItems.FirstOrDefault(bi => bi.ProductId == productId);
+            var result = values.BasketItems.Remove(deletedItem);
+            await SaveBasket(values);
+            return true;        
+        }
+
+        public async Task SaveBasket(BasketTotalDto basketTotalDto)
+        {
+            await _httpClient.PostAsJsonAsync<BasketTotalDto>("baskets", basketTotalDto);
+        }
     }
 }
--- Frontends/ECommerce.WebUı/Services/BasketServices/IBasketService.cs
+++ Frontends/ECommerce.WebUı/Services/BasketServices/IBasketService.cs
@@ -1,9 +1,13 @@
-﻿namespace ECommerce.WebUı.Services.BasketServices
+﻿using ECommerceApp.DtoLayer.BasketDtos;
+
+namespace ECommerce.WebUı.Services.BasketServices
 {
     public interface IBasketService
     {
-        Task<BasketTotalDto> GetBasket(string userId);
+        Task<BasketTotalDto> GetBasket();
         Task SaveBasket(BasketTotalDto basketTotalDto);
         Task DeleteBasket(string userId);
+        Task AddBasketItem(BasketItemDto basketItemDto);
+        Task<bool> RemoveBasketItem(string productId);
     }
 }
