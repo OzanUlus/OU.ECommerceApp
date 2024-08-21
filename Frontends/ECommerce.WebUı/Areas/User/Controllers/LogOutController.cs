d1cc997ef9a2eef7348cbc5f446b7da5f99691d3
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-22 01:34:50 +0300

    user themes

---
+++ Frontends/ECommerce.WebUı/Areas/User/Controllers/LogOutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Areas.User.Controllers
+{
+    public class LogOutController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Frontends/ECommerce.WebUı/Areas/User/Controllers/MyOrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Areas.User.Controllers
+{
+    [Area("User")]
+    public class MyOrderController : Controller
+    {
+        public IActionResult MyOrderList()
+        {
+            return View();
+        }
+    }
+}
