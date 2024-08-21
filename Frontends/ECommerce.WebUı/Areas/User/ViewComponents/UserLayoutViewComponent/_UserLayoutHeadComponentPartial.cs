b84b8ff508d1cfc7798be68ec4004fb8bb865442
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-22 02:25:47 +0300

    themes

---
+++ Frontends/ECommerce.WebUı/Areas/User/ViewComponents/UserLayoutViewComponent/_UserLayoutHeadComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.WebUı.Areas.User.ViewComponents.UserLayoutViewComponent
+{
+    public class _UserLayoutHeadComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { 
+          return View();
+        }
+    }
+}
