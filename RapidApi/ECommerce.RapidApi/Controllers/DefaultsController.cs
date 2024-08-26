d199017e94922c5ca0c7ad3c1c4e7f82d77f5b09
Ozan Ulus <ilkerozanulus@gmail.com>
2024-08-26 15:57:24 +0300

    fixed

---
+++ RapidApi/ECommerce.RapidApi/Controllers/DefaultsController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ECommerce.RapidApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultsController : ControllerBase
+    {
+    }
+}
